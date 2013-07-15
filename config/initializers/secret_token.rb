f35d2bd0e56b0d5ce2540e51e770bc8e1f316b7b
dmanaster <david@manaster.com>
2013-07-15 13:07:33 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HireCrm::Application.config.secret_token = '697889afa6d6356293e956c56c13695aaa3cd9c10fcfa9b20ef0f7290dfc40c779a8abc62e53d9c53141a5896e5b330735d994a869ed0f6048ae61ddc84960e4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+HireCrm::Application.config.session_store :cookie_store, key: '_hire_crm_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# HireCrm::Application.config.session_store :active_record_store
