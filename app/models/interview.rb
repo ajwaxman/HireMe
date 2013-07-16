c310f0ee350f22b6490cdb23838465bb8d3eaec6
dmanaster <david@manaster.com>
2013-07-16 12:06:10 -0400

    Start of state machine.

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -4,4 +4,28 @@ class Interview < ActiveRecord::Base
   belongs_to :company
   belongs_to :job
   belongs_to :student
+
+  include AASM
+
+  aasm do
+    state :open, :initial => true
+    state :in_progress
+    state :closed
+
+    event :email do
+      transitions :from => :open, :to => :in_progress
+    end
+
+    event :clean do
+      transitions :from => :in_progress, :to => :closed
+    end
+
+    event :sleep do
+      transitions :from => [:running, :cleaning], :to => :sleeping
+    end
+  end
+
+end
+
+
 end
