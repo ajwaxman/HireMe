ddfd940fcac763dcc43175c9caa741a0e3fc5a93
chhhris <mr.lake@gmail.com>
2013-07-16 13:42:57 -0400

    comment out aasm events

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -7,20 +7,18 @@ class Interview < ActiveRecord::Base
 
   include AASM
 
-
-
   aasm do
     state :open_status, :initial => true
     state :in_progress
     state :close_status
 
-    event :email do
-      transitions :from => :open, :to => :in_progress
-    end
+    # event :email do
+    #   transitions :from => :open, :to => :in_progress
+    # end
 
-    event :clean do
-      transitions :from => :in_progress, :to => :closed
-    end
+    # event :clean do
+    #   transitions :from => :in_progress, :to => :closed
+    # end
 
   end
 end
