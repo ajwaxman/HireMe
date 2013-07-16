21c826af4ed2baabfaabdedee24d8a54e5fee8cf
cjlwired <carlos.jose.lazo@gmail.com>
2013-07-16 10:55:03 -0400

    Update interview partial

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -14,6 +14,7 @@ class StudentsController < ApplicationController
   # GET /students/1.json
   def show
     @student = Student.find(params[:id])
+    @interviews = @student.interviews
 
     respond_to do |format|
       format.html # show.html.erb
