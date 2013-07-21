02eacfc90688ab468e3088bf27b4489da7b6f146
cjlwired <carlos.jose.lazo@gmail.com>
2013-07-21 17:53:53 -0400

    Fix jobs/interviews NEW action in controller and view.

--- app/controllers/jobs/interviews_controller.rb
+++ app/controllers/jobs/interviews_controller.rb
@@ -1,43 +1,16 @@
 class Jobs::InterviewsController < ApplicationController
 
-	# GET /interviews
-  # GET /interviews.json
-  def index
-    @interviews = Interview.all
+  # Route => /jobs/:id/interviews/new/
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @interviews }
-    end
-  end
-
-  # GET /interviews/1
-  # GET /interviews/1.json
-  def show
-    @interview = Interview.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @interview }
-    end
-  end
-
-  # GET /interviews/new
-  # GET /interviews/new.json
   def new
     @interview = Interview.new
-    @jobs = Job.all
-    @users = User.all
+    @job 			 = Job.find(params[:job_id])
+    @users 		 = User.all
 
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @interview }
     end
-  end
-
-  # GET /interviews/1/edit
-  def edit
-    @interview = Interview.find(params[:id])
   end
 
   # POST /interviews
@@ -62,34 +35,6 @@ class Jobs::InterviewsController < ApplicationController
         format.html { render action: "new" }
         format.json { render json: @interview.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PUT /interviews/1
-  # PUT /interviews/1.json
-  def update
-    @interview = Interview.find(params[:id])
-
-    respond_to do |format|
-      if @interview.update_attributes(params[:interview])
-        format.html { redirect_to @interview, notice: 'Interview was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @interview.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /interviews/1
-  # DELETE /interviews/1.json
-  def destroy
-    @interview = Interview.find(params[:id])
-    @interview.destroy
-
-    respond_to do |format|
-      format.html { redirect_to interviews_url }
-      format.json { head :no_content }
     end
   end
 
