bd786f92c843c33cad7c62bf5e662db192c31cbc
Adam Waxman <awaxman11@gmail.com>
2013-07-23 11:28:44 -0400

    Add role column to user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
 #  password_digest :string(255)
+#  role            :string(255)
 #
 
 class User < ActiveRecord::Base
