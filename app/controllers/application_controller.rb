caab6edee438ae14f8623b2ada8b262315fa1701
chhhris <mr.lake@gmail.com>
2013-07-24 14:30:46 -0400

    remove pipes on application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,7 @@ class ApplicationController < ActionController::Base
 
   private
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-    puts "Current User = #{@current_user}"
+    @current_user = User.find(session[:user_id]) if session[:user_id]
   end
   
   helper_method :current_user
