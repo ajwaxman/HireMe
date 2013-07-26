9414e74f80165179c2146eaed93f05c8b1c0e338
chhhris <mr.lake@gmail.com>
2013-07-26 10:48:10 -0400

    tweaks to dashboard and refactor icon code

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,45 +1,19 @@
 module UsersHelper
 
   def total_students
-  	counter = 0
-  	@users.each {|u| counter += 1 if u.role = "student"}
-  	counter.to_f
+    @users.select {|u| u.role ="student"}.size.to_f
   end
 
   def total_students_interviewing
-  	counter = 0
-  	@users.each {|u| counter += 1 unless u.interviews.empty?}
-  	counter.to_f
+    @users.select {|u| !u.interviews.empty?}.size.to_f
   end
 
   def percentage_not_interviewing
-    not_interviewing = ((total_students - total_students_interviewing) / total_students * 100)
-    not_interviewing
+    ((total_students - total_students_interviewing) / total_students * 100)
   end
 
   def percentage_interviewing
-    100 - percentage_not_interviewing
+    100.5 - percentage_not_interviewing
   end
-
-
-
-# new_old
-    # start_date = DateTime.now - 14
-    # end_date = DateTime.now + 21
-    # array = Array.new
-    # axis_array = Array.new
-    # (start_date..end_date).step(1).each_with_index do |date, index| 
-    #   interview_count = 0
-    #   (date.strftime("%Y-%m-%d")..(date+1).strftime("%Y-%m-%d")).each do |date|
-    #     interview_count += Interview.where(:date => date).count 
-    #   end
-    #   array << [index, interview_count]
-    #   axis_array << [index, date.strftime("%Y-%m-%d")]
-    # end
-    # gon.chart_data = array
-    # gon.axis_data = axis_array
-
-# new_incomplete
-
   
 end
