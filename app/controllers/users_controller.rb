6e32521e3a411b11e357eb78416d91f301ed23fd
chhhris <mr.lake@gmail.com>
2013-07-25 21:01:32 -0400

    admin dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,8 @@ class UsersController < ApplicationController
   def dashboard
     @interviews = Interview.all
     @users = User.all
+
+    # Calendar view data
     json_interviews = []
     @users.each do |user| 
       user.interviews.each do |interview|
@@ -21,16 +23,14 @@ class UsersController < ApplicationController
     end
     gon.interview = json_interviews
         
-
-    weeks_back = 5
-    weeks_forward = 5
+    # Interviews by Week chart data
+    weeks_back = 3
+    weeks_forward = 4
     days_in_week = 7
     start_date = DateTime.now.beginning_of_week(:sunday) - (weeks_back * days_in_week)
     end_date = start_date + ((weeks_back + weeks_forward) * days_in_week)
     array = Array.new
     axis_array = Array.new
-    
-
     (start_date..end_date).step(7).each_with_index do |date, index| 
       interview_count = 0
       (date.strftime("%Y-%m-%d")..(date+7).strftime("%Y-%m-%d")).each do |date|
@@ -39,12 +39,10 @@ class UsersController < ApplicationController
       array << [index, interview_count]
       axis_array << [index, date.strftime("%Y-%m-%d")]
     end
-    
     gon.chart_data = array
     gon.axis_data = axis_array
 
-    
-
+    # boilerplate rails code
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @users }
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,64 @@
 module UsersHelper
+
+  def total_students
+  	counter = 0
+  	@users.each {|u| counter += 1 if u.role = "student"}
+  	counter.to_f
+  end
+
+  def total_students_interviewing
+  	counter = 0
+  	@users.each {|u| counter += 1 unless u.interviews.empty?}
+  	counter.to_f
+  end
+
+  def percentage_not_interviewing
+    not_interviewing = ((total_students - total_students_interviewing) / total_students * 100)
+    not_interviewing
+  end
+
+  def percentage_interviewing
+    100 - percentage_not_interviewing
+  end
+
+
+
+# old
+    weeks_back = 3
+    weeks_forward = 4
+    days_in_week = 7
+    start_date = DateTime.now.beginning_of_week(:sunday) - (weeks_back * days_in_week)
+    end_date = start_date + ((weeks_back + weeks_forward) * days_in_week)
+    array = Array.new
+    axis_array = Array.new
+    (start_date..end_date).step(7).each_with_index do |date, index| 
+      interview_count = 0
+      (date.strftime("%Y-%m-%d")..(date+7).strftime("%Y-%m-%d")).each do |date|
+        interview_count += Interview.where(:date => date).count 
+      end
+      array << [index, interview_count]
+      axis_array << [index, date.strftime("%Y-%m-%d")]
+    end
+    gon.chart_data = array
+    gon.axis_data = axis_array
+
+# new
+    def interviews_past
+      DateTime.now - 14
+
+    end
+
+    def interviews_future
+      DateTime.now + 21
+    end
+
+    def interviews_chart
+      (interviews_past..interviews_future).each_with_index do |date, index| 
+      interview_count = 0
+      (date.strftime("%Y-%m-%d")..(date+7).strftime("%Y-%m-%d")).each do |date|
+        interview_count += Interview.where(:date => date).count 
+      end
+    end
+
+
 end
