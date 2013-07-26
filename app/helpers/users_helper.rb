19325e393425f094b3b4206f9243cf70dc52071e
cjlwired <carlos.jose.lazo@gmail.com>
2013-07-26 11:06:18 -0400

    Refactor interviewing statistics methods in UserHelper.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,15 +1,17 @@
 module UsersHelper
 
+  # Interviewing Statistics
+
   def total_students
-    @users.select {|u| u.role ="student"}.size.to_f
+    @users.count{|u| u.role = "student"}
   end
 
   def total_students_interviewing
-    @users.select {|u| !u.interviews.empty?}.size.to_f
+    @users.count{|u| u.interviews.any?}
   end
 
   def percentage_not_interviewing
-    ((total_students - total_students_interviewing) / total_students * 100)
+    ((total_students - total_students_interviewing) / total_students.to_f * 100)
   end
 
   def percentage_interviewing
