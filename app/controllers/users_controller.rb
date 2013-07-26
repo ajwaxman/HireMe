505640fb8174cc9b946b9d37215f7c05d94d1438
chhhris <mr.lake@gmail.com>
2013-07-26 00:00:18 -0400

    update to dashboard view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,24 +23,72 @@ class UsersController < ApplicationController
     end
     gon.interview = json_interviews
         
-    # Interviews by Week chart data
-    weeks_back = 3
-    weeks_forward = 4
-    days_in_week = 7
-    start_date = DateTime.now.beginning_of_week(:sunday) - (weeks_back * days_in_week)
-    end_date = start_date + ((weeks_back + weeks_forward) * days_in_week)
-    array = Array.new
-    axis_array = Array.new
-    (start_date..end_date).step(7).each_with_index do |date, index| 
-      interview_count = 0
-      (date.strftime("%Y-%m-%d")..(date+7).strftime("%Y-%m-%d")).each do |date|
-        interview_count += Interview.where(:date => date).count 
-      end
-      array << [index, interview_count]
-      axis_array << [index, date.strftime("%Y-%m-%d")]
+    # # Interviews by Week chart data
+    # weeks_back = 3
+    # weeks_forward = 4
+    # days_in_week = 7
+    # start_date = DateTime.now.beginning_of_week(:sunday) - (weeks_back * days_in_week)
+    # end_date = start_date + ((weeks_back + weeks_forward) * days_in_week)
+    # array = Array.new
+    # axis_array = Array.new
+    # (start_date..end_date).step(7).each_with_index do |date, index| 
+    #   interview_count = 0
+    #   (date.strftime("%Y-%m-%d")..(date+7).strftime("%Y-%m-%d")).each do |date|
+    #     interview_count += Interview.where(:date => date).count 
+    #   end
+    #   array << [index, interview_count]
+    #   axis_array << [index, date.strftime("%Y-%m-%d")]
+    # end
+    # gon.chart_data = array
+    # gon.axis_data = axis_array
+
+
+    # start_date = DateTime.now - 6
+    # end_date = DateTime.now + 12
+    # array = Array.new
+    # axis_array = Array.new
+    # (start_date..end_date).step(2).each_with_index do |date, index| 
+    #   interview_count = 0
+    #   (date.strftime("%Y-%m-%d")..(date+2).strftime("%Y-%m-%d")).each do |date|
+    #     interview_count += Interview.where(:date => date).count 
+    #   end
+    #   array << [index, interview_count]
+    #   axis_array << [index, date.strftime("%b-%d")]
+    # end
+    def interviews_past
+      DateTime.now - 6
     end
-    gon.chart_data = array
-    gon.axis_data = axis_array
+
+    def interviews_future
+      DateTime.now + 12
+    end
+
+    def interviews_chart
+      array = Array.new
+          (interviews_past..interviews_future).step(1).each_with_index do |date, index| 
+          interview_count = 0
+          (date.strftime("%Y-%m-%d")..(date+1).strftime("%Y-%m-%d")).each do |date|
+            interview_count += Interview.where(:date => date).count 
+          end
+            array << [index, interview_count]
+        end
+      array
+    end
+
+    def interviews_chart_dates
+      axis_array = Array.new
+      (interviews_past..interviews_future).step(1).each_with_index do |date, index| 
+        interview_count = 0
+        (date.strftime("%Y-%m-%d")..(date+1).strftime("%Y-%m-%d")).each do |date|
+          interview_count += Interview.where(:date => date).count 
+        end
+        axis_array << [index, date.strftime("%b-%d")]
+      end
+      axis_array
+    end
+
+    gon.chart_data = interviews_chart
+    gon.axis_data = interviews_chart_dates
 
     # boilerplate rails code
     respond_to do |format|
