233927a8821d9567947d6655a582fb4d4f44d2df
cjlwired <carlos.jose.lazo@gmail.com>
2013-08-13 17:13:55 -0400

    Remove solr folder and add solr initialization to deploy.rb for Capistrano.

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,3 +30,58 @@ namespace :deploy do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 end
+
+namespace :deploy do
+  # Ask whether to reindex before restarting Passenger
+  task :restart, :roles => :app, :except => {:no_release => true} do
+    solr.reindex if 'y' == Capistrano::CLI.ui.ask("\n\n Should I reindex all models? (anything but y will cancel)")
+    run "touch #{File.join(current_path, 'tmp', 'restart.txt')}"
+  end
+ 
+  desc 'create shared data and pid dirs for Solr'
+  task :setup_solr_shared_dirs do
+    # conf dir is not shared as different versions need different configs
+    %w(data pids).each do |path|
+      run "mkdir -p #{shared_path}/solr/#{path}"    
+    end
+  end
+ 
+  desc 'substitutes current_path/solr/data and pids with symlinks to the shared dirs'
+  task :link_to_solr_shared_dirs do
+    %w(solr/data solr/pids).each do |solr_path|
+      run "rm -fr #{current_path}/#{solr_path}" #removing might not be necessary with proper .gitignore setup
+      run "ln -s #{shared_path}/#{solr_path} #{current_path}/#{solr_path}"
+    end
+  end
+end
+ 
+after 'deploy:setup', 'deploy:setup_solr_shared_dirs'
+# rm and symlinks every time we finished uploading code and symlinking to the new release
+after 'deploy:update', 'deploy:link_to_solr_shared_dirs'
+ 
+ 
+# Tasks to interact with Solr and SunSpot
+namespace :solr do
+  desc "start solr"
+  task :start, :roles => :app, :except => { :no_release => true } do
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec rake sunspot:solr:start"
+  end
+  desc "stop solr"
+  task :stop, :roles => :app, :except => { :no_release => true } do
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec rake sunspot:solr:stop"
+  end
+ 
+  desc "stop solr, remove data, start solr, reindex all records"
+  task :hard_reindex, :roles => :app do
+    stop
+    run "rm -rf #{shared_path}/solr/data/*"
+    start
+    reindex
+  end
+ 
+ 
+  desc "simple reindex" #note the yes | reindex to avoid the nil.chomp error
+  task :reindex, roles: :app do
+    run "cd #{current_path} && yes | RAILS_ENV=#{rails_env} bundle exec rake sunspot:solr:reindex"
+  end
+end
