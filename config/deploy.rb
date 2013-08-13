7288e03d0c2ca1d53caa48ff7bd044947bbc6cb1
cjlwired <carlos.jose.lazo@gmail.com>
2013-08-13 13:47:00 -0400

    Quick fix to Capistrano deploy file.

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,8 @@ set :use_sudo, false
 set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
+default_run_options[:pty] = true
+
 role :web, "192.241.176.112"                          # Your HTTP server, Apache/etc
 role :app, "192.241.176.112"                          # This may be the same as your `Web` server
 # role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
