3163581fa45d61e30ccf1d2e679739f50ca39057
cjlwired <carlos.jose.lazo@gmail.com>
2013-08-13 20:34:59 -0400

    Add files to rake file.

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,11 +13,10 @@ default_run_options[:pty] = true
 
 role :web, "192.241.176.112"                          # Your HTTP server, Apache/etc
 role :app, "192.241.176.112"                          # This may be the same as your `Web` server
-# role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-# role :db,  "your slave db-server here"
 
-# if you want to clean up old releases on each deploy uncomment this:
-after "deploy:restart", "deploy:cleanup"
+# Load assets here and create symlinks.
+before 'deploy:assets:precompile', 'deploy:symlink_config'
+
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
@@ -28,5 +27,10 @@ namespace :deploy do
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+
+  task :symlink_config, :roles => :app do 
+    run "ln -nfs #{shared_path}/application.yml #{current_release}/config/application.yml"
+    run "ln -nfs #{shared_path}/database.yml #{current_release}/config/database.yml"
   end
 end
