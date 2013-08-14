87d37f29490260d437bc131740dadb61c4667c87
cjlwired <carlos.jose.lazo@gmail.com>
2013-08-14 11:12:49 -0400

    made capistrano task to copy css files to public manually

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,7 @@ role :app, "192.241.176.112"                          # This may be the same as 
 
 # Load assets here and create symlinks.
 # after 'deploy:update_code','deploy:symlink_config'
-
+after 'deploy:update_code','deploy:copy_themed_css'
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
@@ -31,5 +31,9 @@ namespace :deploy do
   task :symlink_config, :roles => :app do 
     run "ln -nfs #{shared_path}/application.yml #{current_release}/config/application.yml"
     run "ln -nfs #{shared_path}/production.sqlite3 #{current_release}/db/production.sqlite3"
+  end
+
+  task :copy_themed_css do
+  	run "cp #{current_release}/vendor/assets/perfectum_dashboard_1_0_5/* public"
   end
 end
