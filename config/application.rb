94cde4619267de87614f9a884508635ebef388fb
cjlwired <carlos.jose.lazo@gmail.com>
2013-08-14 10:42:34 -0400

    Update asset pipeline include path.

--- config/application.rb
+++ config/application.rb
@@ -55,7 +55,7 @@ module HireCrm
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.paths << "#{Rails.root}/vendor/assets/"
+    # config.assets.paths << "#{Rails.root}/vendor/assets/"
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
