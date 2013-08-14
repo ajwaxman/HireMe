d2814b37411bc523a9225aff1e3212bae18fccfd
cjlwired <carlos.jose.lazo@gmail.com>
2013-08-14 10:16:17 -0400

    blakes commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,35 +10,3 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
-//= require jquery_ujs
-//= require ../../../vendor/assets/perfectum_dashboard_1_0_5/js/bootstrap-switch.js
-//= require ../../../vendor/assets/perfectum_dashboard_1_0_5/js/jquery-1.9.1.min.js
-//= require ../../../vendor/assets/perfectum_dashboard_1_0_5/js/jquery-migrate-1.0.0.min.js
-//= require ../../../vendor/assets/perfectum_dashboard_1_0_5/js/jquery-ui-1.10.0.custom.min.js
-//= require ../../../vendor/assets/perfectum_dashboard_1_0_5/js/jquery.ui.touch-punch.js
-//= require ../../../vendor/assets/perfectum_dashboard_1_0_5/js/bootstrap.min.js
-//= require ../../../vendor/assets/perfectum_dashboard_1_0_5/js/jquery.cookie.js
-//= require ../../../vendor/assets/perfectum_dashboard_1_0_5/js/fullcalendar.min.js
-//= require ../../../vendor/assets/perfectum_dashboard_1_0_5/js/jquery.dataTables.min.js
-//= require ../../../vendor/assets/perfectum_dashboard_1_0_5/js/excanvas.js
-//= require ../../../vendor/assets/perfectum_dashboard_1_0_5/js/jquery.flot.min.js
-//= require ../../../vendor/assets/perfectum_dashboard_1_0_5/js/jquery.flot.pie.min.js
-//= require ../../../vendor/assets/perfectum_dashboard_1_0_5/js/jquery.flot.stack.js
-//= require ../../../vendor/assets/perfectum_dashboard_1_0_5/js/jquery.flot.resize.min.js
-//= require ../../../vendor/assets/perfectum_dashboard_1_0_5/js/jquery.chosen.min.js
-//= require ../../../vendor/assets/perfectum_dashboard_1_0_5/js/jquery.uniform.min.js
-//= require ../../../vendor/assets/perfectum_dashboard_1_0_5/js/jquery.cleditor.min.js
-//= require ../../../vendor/assets/perfectum_dashboard_1_0_5/js/jquery.noty.js
-//= require ../../../vendor/assets/perfectum_dashboard_1_0_5/js/jquery.elfinder.min.js
-//= require ../../../vendor/assets/perfectum_dashboard_1_0_5/js/jquery.raty.min.js
-//= require ../../../vendor/assets/perfectum_dashboard_1_0_5/js/jquery.iphone.toggle.js
-//= require ../../../vendor/assets/perfectum_dashboard_1_0_5/js/jquery.uploadify-3.1.min.js
-//= require ../../../vendor/assets/perfectum_dashboard_1_0_5/js/jquery.gritter.min.js
-//= require ../../../vendor/assets/perfectum_dashboard_1_0_5/js/jquery.imagesloaded.js
-//= require ../../../vendor/assets/perfectum_dashboard_1_0_5/js/jquery.masonry.min.js
-//= require ../../../vendor/assets/perfectum_dashboard_1_0_5/js/jquery.knob.js
-//= require ../../../vendor/assets/perfectum_dashboard_1_0_5/js/jquery.sparkline.min.js
-//= require ../../../vendor/assets/bootstrap-timepicker/js/bootstrap-timepicker.js
-//= require ../../../vendor/assets/perfectum_dashboard_1_0_5/js/custom.js
-//= require add_questions.js
