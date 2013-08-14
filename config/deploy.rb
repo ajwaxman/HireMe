3dc9a2a526fdabe6528ba29b8754934e3fc3f250
cjlwired <carlos.jose.lazo@gmail.com>
2013-08-14 19:26:08 -0400

    Update deploy.rb to write crontab via whenever when updated.

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,7 @@ role :app, "192.241.176.112"                          # This may be the same as 
 
 # Load assets here and create symlinks.
 # after 'deploy:update_code','deploy:symlink_config'
-after 'deploy:update_code','deploy:copy_styling_elements'
+after 'deploy:update_code','deploy:setup_server'
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
@@ -33,10 +33,15 @@ namespace :deploy do
     run "ln -nfs #{shared_path}/production.sqlite3 #{current_release}/db/production.sqlite3"
   end
 
-  task :copy_styling_elements do
+  task :setup_server do
+
+  	# Copy all styling elements from database.
   	run "cp #{current_release}/vendor/assets/perfectum_dashboard_1_0_5/css/* #{current_release}/public"
   	run "cp #{current_release}/vendor/assets/perfectum_dashboard_1_0_5/css/* #{current_release}/public/assets"
 		run "cp -rf #{current_release}/vendor/assets/perfectum_dashboard_1_0_5/img/* #{current_release}/public/img"
   	run "cp -rf #{current_release}/vendor/assets/perfectum_dashboard_1_0_5/img/* #{current_release}/public/assets/img"	
+
+  	# Update crontab if there are any changes.
+  	run "cd #{release_path} && whenever --update-crontab #{application}"
   end
 end
