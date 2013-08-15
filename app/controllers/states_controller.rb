b5083508809f80f55b4160c5b451d752a9de10ee
cjlwired <carlos.jose.lazo@gmail.com>
2013-08-15 06:34:38 -0400

    Remove old like/unlike functions from controller.

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -1,34 +1,5 @@
 class StatesController < ApplicationController
   
-  # Like and Unlike a job as a logged in User.
-
-  def like_company
-    job  = Job.find(params[:id])
-    user = current_user
-
-    relationship = Relationship.find_or_create_by_user_id_and_job_id_and_company_id(	
-    											:user_id => user.id, 
-    											:job_id => job.id, 
-    											:company_id => job.company_id)
-    relationship.aasm_state = "like"
-
-    Record.write_record(relationship) if relationship.save
-    
-    redirect_to job_path(job), :flash => { :success => "You've liked the #{job.title} position at #{job.company.name}."}
-  end
-
-  def unlike_company
-    job  = Job.find(params[:id])
-    user = current_user
-    
-    relationship = Relationship.find_by_user_id_and_job_id_and_company_id(user.id, job.id, job.company_id)
-    relationship.aasm_state = "start"
-    
-    Record.write_record(relationship) if relationship.save
-    
-    redirect_to job_path(job), :flash => { :notice => "You've unliked the #{job.title} position at #{job.company.name}."}
-  end
-
   # Check to see if Relationship exists 
   # If the relationship exists, find the relationship
   # If not, allow for a button to create a relationship
@@ -37,6 +8,7 @@ class StatesController < ApplicationController
   #################
   ## Constraints ##
   #################
+  
   # => On the job page, no relationship initially exists
   # => params[:id] is different on the jobs page and the interview page
 
