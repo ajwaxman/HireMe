c80a22f353e8db5c3ff4b21fd0a666cba04e3fdd
cjlwired <carlos.jose.lazo@gmail.com>
2013-08-14 20:53:38 -0400

    Fix record writing with new production code.

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -79,7 +79,7 @@ class StatesController < ApplicationController
   def change_state
     r = find_relationship
     r.send(params["event"])
-    r.save 
+    Record.write_record(r) if r.save
   end
 
   def create_relationship_and_like
@@ -94,86 +94,6 @@ class StatesController < ApplicationController
     elsif params[:controller_name] == "interviews"
       redirect_to interview_path(Interview.find(params[:id]))
     end
-  end
-
-  # Like and Unlike companies from the relationship page.
-
-  def rel_like_company
-    relationship = Relationship.find(params[:id])
-    job          = relationship.job
-    user         = relationship.user
-    
-    relationship.aasm_state = "like"
-    Record.write_record(relationship) if relationship.save
-
-    redirect_to job_path(job), :flash => { :success => "You've liked the #{job.title} position at #{job.company.name}."}
-  end
-
-  def rel_unlike_company
-    relationship = Relationship.find(params[:id])
-    job          = relationship.job
-    user         = relationship.user
-    
-    relationship.aasm_state = "start"
-    Record.write_record(relationship) if relationship.save
-
-    redirect_to job_path(job), :flash => { :notice => "You've unliked the #{job.title} position at #{job.company.name}."}
-  end
-
-  def rel_company_declined
-    user = current_user
-    interview = Interview.find(params[:id])
-    job = interview.job
-    relationship = Relationship.find_by_user_id_and_job_id(user.id,job.id) 
-    relationship.aasm_state = "company_decline"
-    relationship.save if relationship
-    Record.write_record(relationship) if relationship.save
-    redirect_to interview_path(interview), :flash => { :notice => "#{relationship.company.name} declined to continue discussions."}
-  end
-
-  def rel_user_declined
-    user = current_user
-    interview = Interview.find(params[:id])
-    job = interview.job
-    relationship = Relationship.find_by_user_id_and_job_id(user.id,job.id) 
-    relationship.aasm_state = "user_decline"
-    relationship.save if relationship
-    Record.write_record(relationship) if relationship.save
-    redirect_to interview_path(interview), :flash => { :notice => "You declined to continue discussing the #{relationship.job.title} position at #{relationship.company.name}."}
-  end
-
-  def rel_offer_received
-    user = current_user
-    interview = Interview.find(params[:id])
-    job = interview.job
-    relationship = Relationship.find_by_user_id_and_job_id(user.id,job.id) 
-    relationship.aasm_state = "offer_received"
-    relationship.save if relationship
-    Record.write_record(relationship) if relationship.save
-    redirect_to interview_path(interview), :flash => { :success => "You received an offer for the #{relationship.job.title} position at #{relationship.company.name}. Congrats!"}
-  end
-
-  def accept_offer
-    user = current_user
-    interview = Interview.find(params[:id])
-    job = interview.job
-    relationship = Relationship.find_by_user_id_and_job_id(user.id,job.id) 
-    relationship.aasm_state = "offer_accepted"
-    relationship.save
-    Record.write_record(relationship)
-    
-    redirect_to user_path(user), :flash => { :success => "You were offered and accepted the #{relationship.job.title} position at #{relationship.company.name}. Congrats!"}
-  end
-
-  def reject_offer
-    user = current_user
-    interview = Interview.find(params[:id])
-    job = interview.job
-    relationship = Relationship.find_by_user_id_and_job_id(user.id,job.id) 
-    relationship.aasm_state = "offer_declined"
-    relationship.save if relationship
-    Record.write_record(relationship) if relationship.save
-    redirect_to user_path(user), :flash => { :notice => "You declined the #{relationship.job.title} position at #{relationship.company.name}. To greener pastures!"}
   end
 
 end
